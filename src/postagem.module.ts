9d6fd170ae779b5568eadbbf08269a3f36b171c9
Phcode007 <paulobelarmino047@gmail.com>
2025-09-22 23:05:06 -0300

    Atualização do module, criação do controller e do service

--- src/postagem.module.ts
+++ src/postagem.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostagemController } from './postagem/controllers/postagem.controller';
 import { Postagem } from './postagem/entities/postagem.entity';
+import { PostagemService } from './postagem/services/postagem.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Postagem])],
-  providers: [],
-  controllers: [],
+  providers: [PostagemService],
+  controllers: [PostagemController],
   exports: [],
 })
 export class PostagemModule {}
