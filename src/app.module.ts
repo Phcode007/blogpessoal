80cc0b513e7f94fa0bad34f9d6b032fefa839f1f
Phcode007 <paulobelarmino047@gmail.com>
2025-10-04 10:45:29 -0300

    Arquivos reorganizados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Postagem } from './postagem/entities/postagem.entity';
-import { PostagemModule } from './postagem.module';
+import { PostagemModule } from './postagem/postagem.module';
 import { Tema } from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module';
 import { AuthModule } from './auth/auth.module';
--- src/postagem/postagem.module.ts
+++ src/postagem/postagem.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TemaModule } from './tema/tema.module';
-import { PostagemController } from './postagem/controllers/postagem.controller';
-import { Postagem } from './postagem/entities/postagem.entity';
-import { PostagemService } from './postagem/services/postagem.service';
+import { TemaModule } from '../tema/tema.module';
+import { PostagemController } from './controllers/postagem.controller';
+import { Postagem } from './entities/postagem.entity';
+import { PostagemService } from './services/postagem.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Postagem]), TemaModule],
